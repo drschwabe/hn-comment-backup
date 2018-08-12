eb0a80f6de94da7b21b29fb0e604ca74483ee225
Derrick Schwabe <admin@nerdbusiness.com>
2018-08-12 16:10:24 +0900

    Use config for port and user, cleanup init / add comment s

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-var express = require('express'), 
-    app = express(), 
-    fs = require('fs')
-    //cheerio = require('cheerio')
-
-app.listen(2984, () => console.log('HN comment backup available at: \nhttp://localhost:2984'))
-
-var nightmare = require('nightmare')
+//## HN Comment Backup ###
 
 
-Nightmare = require('nightmare')
+//Use config (./default.json or local.json to override)
+//for port and user values
+const config = require('config')
+var port = config.get('port')
+var user = config.get('user')
+
+//Start an Express server
+const express = require('express')
+const app = express()
+app.listen(port, () => console.log('HN comment backup available at: \nhttp://localhost:' + port))
+
+//Start Nightmare which will crawl news.ycombinator.com: 
+const Nightmare = require('nightmare')
 var nightmare = new Nightmare({
   show: false, 
   alwaysOnTop : false
@@ -18,28 +23,24 @@ var nightmare = new Nightmare({
   if(errorStack) console.log(errorStack)  
 }) 
 
+//Write file after we receive result...
+const fs = require('fs')
+
+//go!
+console.log('fetching/parsing profile page...') 
 nightmare
-.goto('https://news.ycombinator.com/threads?id=mrschwabe')
+.goto('https://news.ycombinator.com/threads?id=' + config.get('user'))
 .wait(1000)
 .end()
-.evaluate(()=> { 
-  //return document.documentElement.outerHTML
-  return document.body.outerHTML
-})
+.evaluate(()=> document.body.outerHTML)
 .then((result) => {
-  console.log(result)
-
+  console.log('result downloaded')
   const template = fs.readFileSync('./template.html')
-
   fs.writeFile('./page.html', template + result + + '</body></html>', 'utf8', (err) => {
     if(err) return console.log(err)
+    console.log('page.html written.')      
   })
-
 })
 
 //find more link, click it
 //if no more link - we done ! 
-
-
-
-
